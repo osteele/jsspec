fe1b9d1c5997c2b544fe32968f8d600040542db5
Alan Kang <jania902@gmail.com>
2007-07-13 03:04:17 +0000

    git-svn-id: http://jsspec.googlecode.com/svn/trunk@8 92187c49-0d35-0410-b305-2f54cc65526e

---
+++ src/ExampleSpec.js
@@ -0,0 +1,5 @@
+describe('Simple Arithmatic', {
+	'1 plus 1 should be 2': function() {
+		x.x();
+	}
+});
--- src/JSSpec.js
+++ src/JSSpec.js
@@ -1,95 +1,105 @@
-JSSpec = {};
+// Creating namespace
+JSSpec = {}
+
+JSSpec.Browser = {
+	Trident: navigator.appName == "Microsoft Internet Explorer",
+	Webkit: navigator.userAgent.indexOf('AppleWebKit/') > -1,
+	Gecko: navigator.userAgent.indexOf('Gecko') > -1 && navigator.userAgent.indexOf('KHTML') == -1,
+	Presto: navigator.appName == "Opera"
+}
 
 JSSpec.behaviors = [];
 
-Describe = function(context, entries) {
+
+
+describe = function(context, entries) {
 	JSSpec.behaviors.push(new JSSpec.Behavior(context, entries));
 };
 
 
 
-
+/**
+ * Behavior contains its own context and examples.
+ */
 JSSpec.Behavior = function(context, entries) {
 	this.context = context;
-	this.examples = entries;
+	this.examples = this.generateExamplesFromEntries(entries);
 };
+
+JSSpec.Behavior.prototype.generateExamplesFromEntries = function(entries) {
+	// TODO: extract out "before" and "after" entries
+	var examples = [];
+	for(var name in entries) {
+		examples.push(new JSSpec.Example(name, entries[name]));
+	}
+	return examples;
+};
+
 JSSpec.Behavior.prototype.run = function() {
-	for(var name in this.examples) {
-		JSSpec.Behavior._pass = 1;
-		this.examples[name]();
-
-		JSSpec.Behavior._pass = 2;
-		this.examples[name]();
+	for(var i = 0; i < this.examples.length; i++) {
+		this.examples[i].run();
 	}
 }
 
 
-JSSpec.Runner = function(behaviors, logger) {
-	this.behaviors = behaviors;
-	this.logger = logger;
-};
 
-JSSpec.Runner.prototype.pass = function() {
-	document.title += "."
+/**
+ * Example class
+ */
+JSSpec.Example = function(name, fn) {
+	this.name = name;
+	this.fn = fn;
+	this.pass = 1;
 }
 
-JSSpec.Runner.prototype.fail = function(expected, actual) {
-	document.title += "F"
+JSSpec.Example.prototype.error = function(message, sourceUrl, lineNumber) {
+	alert([message, sourceUrl, lineNumber]);
 }
 
-JSSpec.Runner.prototype.error = function(e) {
-	document.title += "E"
-}
+JSSpec.Example.prototype.parsePrestoException = function(ex) {
+	// TODO: Opera cannot handle embeded source correctly. And IE only handles embeded source.
+	var str = ex.toString();
+	var m = str.match(/message\:.+on line (\d+)\: (.+)$/m);
+	var message = m[0];
+	var lineNumber = m[1];
 
-JSSpec.Runner.prototype.run = function() {
-	this.addBddMethods();
+	m = str.match(/Line \d+ of linked script (.+)$/m);
+	var fileName = m[1];
 	
-	for(var i = 0; i < this.behaviors.length; i++) {
-		this.behaviors[i].run();
-	}
-};
+	return {message:message, fileName:fileName, lineNumber:lineNumber};
+}
 
-JSSpec.Runner.prototype.addBddMethods = function() {
-	String.prototype.should = function(expected) {
-		if(JSSpec.Behavior._pass == 1) {
-			
-		} else {
-			
+JSSpec.Example.prototype.run = function() {
+	if(JSSpec.Browser.Trident) {
+		// In IE, exception should be handled at onerror handler.
+		// It is the only known way to get a valid line number.
+		JSSpec.currentExample = this;
+		this.fn();
+	} else {
+		try {
+			this.fn();
+		} catch(ex) {
+			if(JSSpec.Browser.Presto) ex = this.parsePrestoException(ex);
+			this.error(ex.message, ex.fileName, ex.lineNumber);
 		}
-	};
+	}
 }
 
 
-JSSpec.Logger = function() {
-	this.summary = this.createSummary();
-	this.logTable = this.createLogTable();
-};
-JSSpec.Logger.prototype.createSummary = function() {
-	var id = 'JSSpec_summary';
-	var summary = document.getElementById(id);
-	if(!summary) {
-		summary = document.createElement('DIV');
-		summary.id = id;
-		document.body.appendChild(summary);
-	}
-	
-	return summary;
-};
-JSSpec.Logger.prototype.createLogTable = function() {
-	var id = 'JSSpec_logTable';
-	var logTable = document.getElementById(id);
-	if(!logTable) {
-		var logTableContainer = document.createElement('DIV');
-		logTableContainer.innerHTML = '<table></table>';
-		var logTable = logTableContainer.firstChild;
-		logTable.id = id;
-		document.body.appendChild(logTable);
-	}
-	
-	return logTable;
-};
 
-window.onload = function() {
-	var runner = new JSSpec.Runner(JSSpec.behaviors, new JSSpec.Logger());
-	runner.run();
-};
+function main() {
+	if(JSSpec.Browser.Trident) {
+		window.onerror = function(ex, sourceUrl, lineNumber) {
+			JSSpec.currentExample.error(ex, sourceUrl, lineNumber);
+			return true;
+		}
+	}
+	
+	window.onload = function() {
+		for(var i = 0; i < JSSpec.behaviors.length; i++) {
+			JSSpec.behaviors[i].run();
+		}
+	}
+}
+
+main();
