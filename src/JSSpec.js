b565d8b84a9a63770209a27d0924ffa97529f15a
Alan Kang <jania902@gmail.com>
2007-07-13 10:10:36 +0000

    git-svn-id: http://jsspec.googlecode.com/svn/trunk@13 92187c49-0d35-0410-b305-2f54cc65526e

--- src/JSSpec.js
+++ src/JSSpec.js
@@ -1,5 +1,11 @@
-// Creating namespace
-JSSpec = {}
+var JSSpec = {
+	behaviors: [],
+	
+	example: null,
+	behavior: null,
+	runner: null,
+	logger: null
+}
 
 JSSpec.Browser = {
 	Trident: navigator.appName == "Microsoft Internet Explorer",
@@ -8,13 +14,102 @@ JSSpec.Browser = {
 	Presto: navigator.appName == "Opera"
 }
 
-JSSpec.behaviors = [];
-
-
-
 describe = function(context, entries) {
 	JSSpec.behaviors.push(new JSSpec.Behavior(context, entries));
-};
+}
+
+
+
+/**
+ * Example
+ */
+JSSpec.Example = function(name, fn) {
+	this.name = name;
+	this.fn = fn;
+}
+
+JSSpec.Example.prototype.run = function() {
+	JSSpec.example = this;
+	JSSpec.logger.startExample(this);
+	
+	if(JSSpec.Browser.Trident) {
+		// First pass
+		this.fn();
+		this.finished();
+	} else {
+		// First pass
+		try {
+			this.fn();
+			this.finished();
+			return;
+		} catch(ex) {
+			if(ex != "AssertionFailure") {
+				this.recordError(ex.message, ex.fileName, ex.lineNumber);
+				this.finished();
+				return;
+			}
+		}
+		
+		// Second pass
+		try {
+			this.fn();
+		} catch(ex) {
+			this.recordFailure(ex.fileName, ex.lineNumber);
+			this.finished();
+			return;
+		}
+	}
+}
+
+JSSpec.Example.prototype.recordError = function(message, fileName, lineNumber) {
+	this.error = {};
+	this.error.message = message;
+	this.error.fileName = fileName;
+	this.error.lineNumber = lineNumber;
+}
+	
+JSSpec.Example.prototype.recordFailure = function(fileName, lineNumber) {
+	this.failure.fileName = fileName;
+	this.failure.lineNumber = lineNumber;
+}
+	
+JSSpec.Example.prototype.finished = function() {
+	JSSpec.logger.endExample(this);
+	window.setTimeout(JSSpec.behavior.run.bind(JSSpec.behavior));
+}
+
+
+
+/**
+ * Behavior
+ */
+JSSpec.Behavior = function(context, entries) {
+	this.context = context;
+	this.examples = this.makeExamples(entries);
+	this.index = 0;
+}
+
+JSSpec.Behavior.prototype.makeExamples = function(entries) {
+	var examples = [];
+	for(name in entries) {
+		examples.push(new JSSpec.Example(name, entries[name]));
+	}
+	return examples;
+}
+
+JSSpec.Behavior.prototype.run = function() {
+	JSSpec.behavior = this;
+
+	if(this.index == 0) {
+		JSSpec.logger.startBehavior(this);
+		this.examples[this.index++].run();
+	} else if(this.index >= this.examples.length) {
+		JSSpec.logger.endBehavior(this);
+		JSSpec.runner.run();
+	} else {
+		this.examples[this.index++].run();
+	}
+}
 
 
 
@@ -22,139 +117,112 @@ describe = function(context, entries) {
  * Runner
  */
 JSSpec.Runner = function() {
-	this.behavoirs = JSSpec.behaviors;
-}
-JSSpec.Runner.prototype.run = function(bindex, eindex) {
-	bindex = bindex || 0;
-	eindex = eindex || 0;
-	
-	for(var i = bindex; i < JSSpec.behaviors.length; i++) {
-		JSSpec.bindex = i;
-		JSSpec.behaviors[i].run(i == bindex ? eindex : 0);
-	}
+	this.behaviors = JSSpec.behaviors;
+	this.index = 0;
 }
 
+JSSpec.Runner.prototype.run = function() {
+	JSSpec.runner = this;
 
-
-/**
- * Behavior contains its own context and examples.
- */
-JSSpec.Behavior = function(context, entries) {
-	this.context = context;
-	this.examples = this.generateExamplesFromEntries(entries);
-};
-
-JSSpec.Behavior.prototype.generateExamplesFromEntries = function(entries) {
-	// TODO: extract out "before" and "after" entries
-	var examples = [];
-	for(var name in entries) {
-		examples.push(new JSSpec.Example(name, entries[name]));
-	}
-	return examples;
-};
-
-JSSpec.Behavior.prototype.run = function(eindex) {
-	eindex = eindex || 0;
-	for(var i = eindex; i < this.examples.length; i++) {
-		JSSpec.eindex = i;
-		this.examples[i].run();
-	}
-}
-
-
-
-/**
- * Example class
- */
-JSSpec.Example = function(name, fn) {
-	this.name = name;
-	this.fn = fn;
-	
-	this.secondPass = false;
-	this.assertionIndex = 0;
-	this.failure = null;
-	this.exception = null;
-}
-
-JSSpec.Example.prototype.recordFail = function(index, expected, actual) {
-	this.failure = {index:index, expected:expected, actual:actual};
-}
-JSSpec.Example.prototype.recordException = function(message, sourceUrl, lineNumber) {
-	this.exception = {message:message, sourceUrl:sourceUrl, lineNumber:lineNumber};
-}
-
-JSSpec.Example.prototype.parsePrestoException = function(ex) {
-	// TODO: Opera cannot handle embeded source correctly. And IE only handles embeded source.
-	var str = ex.toString();
-	var m = str.match(/message\:.+on line (\d+)\: (.+)$/m);
-	var message = m[0];
-	var lineNumber = m[1];
-
-	m = str.match(/Line \d+ of linked script (.+)$/m);
-	var fileName = m[1];
-	
-	return {message:message, fileName:fileName, lineNumber:lineNumber};
-}
-
-JSSpec.Example.prototype.run = function() {
-	this.addBddMethods();
-
-	// First pass
-	this.fn();
-	if(!this.failure) return;
-	
-	// Second pass to figure out exception info such as line number.
-	this.secondPass = true;
-	if(JSSpec.Browser.Trident) {
-		// In IE, exception should be handled at onerror handler.
-		// It is the only known way to get a valid line number.
-		JSSpec.currentExample = this;
-		this.fn();
+	if(this.index == 0) {
+		JSSpec.logger.startRunner(this);
+		this.behaviors[this.index++].run();
+	} else if(this.index >= this.behaviors.length) {
+		JSSpec.logger.endRunner(this);
 	} else {
-		try {
-			this.fn();
-		} catch(ex) {
-			if(JSSpec.Browser.Presto) ex = this.parsePrestoException(ex);
-			this.recordException(ex.message, ex.fileName, ex.lineNumber);
-		}
+		this.behaviors[this.index++].run();
 	}
-	
-	console.log(this);
 }
 
-JSSpec.Example.prototype.addBddMethods = function() {
+
+
+/**
+ * Logger
+ */
+JSSpec.Logger = function() {
+	//TODO
+}
+JSSpec.Logger.prototype.startRunner = function(runner) {
+	console.log("start runner");
+	console.log(runner);
+}
+JSSpec.Logger.prototype.endRunner = function(runner) {
+	console.log("end runner");
+	console.log(runner);
+}
+JSSpec.Logger.prototype.startBehavior = function(behavior) {
+	console.log("start behavior");
+	console.log(behavior);
+}
+JSSpec.Logger.prototype.endBehavior = function(behavior) {
+	console.log("end behavior");
+	console.log(behavior);
+}
+JSSpec.Logger.prototype.startExample = function(example) {
+	console.log("start example");
+	console.log(example);
+}
+JSSpec.Logger.prototype.endExample = function(example) {
+	console.log("end example");
+	console.log(example);
+}
+
+
+
+/**
+ * BDD Extentions
+ */
+String.prototype.should = function() {
 	var self = this;
-	var targets = [String.prototype, Date.prototype, Number.prototype, Array.prototype];
+	var secondPass = !!JSSpec.example.failure;
 	
-	for(var i = 0; i < targets.length; i++) {
-		targets[i].should = function() {
-			if(self.secondPass && self.failure.index == self.assertionIndex) return {};
-			
-			return {
-				be: function(expected) {
-					if(this != expected) self.recordFail(self.assertionIndex, expected, this);
-					self.assertionIndex++;
-				}.bind(this)
+	return secondPass ?
+		{} :
+		{
+			be: function(expected) {
+				if(self == expected) return;
+				JSSpec.example.failure = {
+					expected:expected,
+					actual:self,
+					message: "Expected [" + expected + "] but [" + self + "]"
+				};
+				throw "AssertionFailure"
 			}
 		}
-	}
 }
 
 
 
-function main() {
-	if(JSSpec.Browser.Trident) {
-		window.onerror = function(ex, sourceUrl, lineNumber) {
-			JSSpec.currentExample.recordException(ex, sourceUrl, lineNumber);
-			JSSpec.runner.run(JSSpec.bindex, JSSpec.eindex + 1);
-			return true;
+if(JSSpec.Browser.Trident) {
+	window.onerror = function(ex, sourceUrl, lineNumber) {
+		var example = JSSpec.example;
+		
+		if(ex == "AssertionFailure") {
+			// Second pass
+			window.setTimeout(example.fn, 0);
+		} else if(example.failure) {
+			example.recordFailure(sourceUrl, lineNumber);
+			example.finished();
+		} else {
+			example.recordError(ex.toString(), sourceUrl, lineNumber);
+			example.finished();
 		}
-	}
-	
-	window.onload = function() {
-		JSSpec.runner = new JSSpec.Runner();
-		JSSpec.runner.run();
+		
+		return true;
 	}
 }
 
-main();
+
+window.console = {
+	log: function(msg) {
+		var div = document.createElement("div");
+		div.innerHTML = msg;
+		document.body.appendChild(div);
+	}
+}
+window.onload = function() {
+	JSSpec.logger = new JSSpec.Logger();
+	JSSpec.runner = new JSSpec.Runner();
+	
+	JSSpec.runner.run();
+}
