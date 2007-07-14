81f4fd86963ecee43cbd2d0a15dc262369ab78bc
Alan Kang <jania902@gmail.com>
2007-07-14 12:28:42 +0000

    git-svn-id: http://jsspec.googlecode.com/svn/trunk@14 92187c49-0d35-0410-b305-2f54cc65526e

--- src/JSSpec.js
+++ src/JSSpec.js
@@ -85,8 +85,30 @@ JSSpec.Example.prototype.finished = function() {
  */
 JSSpec.Behavior = function(context, entries) {
 	this.context = context;
+	this.extractOutSpecialEntries(entries);
 	this.examples = this.makeExamples(entries);
 	this.index = 0;
+}
+
+JSSpec.Behavior.prototype.extractOutSpecialEntries = function(entries) {
+	for(name in entries) {
+		if(name == 'before' || name == 'before each') {
+			this.beforeEach = entries[name];
+		} else if(name == 'before all') {
+			this.beforeAll = entries[name];
+		} else if(name == 'after' || name == 'after each') {
+			this.afterEach = entries[name];
+		} else if(name == 'after all') {
+			this.afterAll = entries[name];
+		}
+	}
+	
+	delete entries['before'];
+	delete entries['before each'];
+	delete entries['before all'];
+	delete entries['after'];
+	delete entries['after each'];
+	delete entries['after all'];
 }
 
 JSSpec.Behavior.prototype.makeExamples = function(entries) {
@@ -99,16 +121,26 @@ JSSpec.Behavior.prototype.makeExamples = function(entries) {
 
 JSSpec.Behavior.prototype.run = function() {
 	JSSpec.behavior = this;
-
+	
+	var example = this.examples[this.index];
+	
 	if(this.index == 0) {
 		JSSpec.logger.startBehavior(this);
-		this.examples[this.index++].run();
+		if(this.beforeAll) {
+			this.beforeAll();
+		}
+		window.setTimeout(example.run.bind(example));
 	} else if(this.index >= this.examples.length) {
 		JSSpec.logger.endBehavior(this);
-		JSSpec.runner.run();
+		if(this.afterAll) {
+			this.afterAll();
+		}
+		window.setTimeout(JSSpec.runner.run.bind(JSSpec.runner));
 	} else {
-		this.examples[this.index++].run();
+		window.setTimeout(example.run.bind(example));
 	}
+	
+	this.index++;
 }
 
 
@@ -124,14 +156,18 @@ JSSpec.Runner = function() {
 JSSpec.Runner.prototype.run = function() {
 	JSSpec.runner = this;
 
+	var behavior = this.behaviors[this.index];
+	
 	if(this.index == 0) {
 		JSSpec.logger.startRunner(this);
-		this.behaviors[this.index++].run();
+		window.setTimeout(behavior.run.bind(behavior), 0);
 	} else if(this.index >= this.behaviors.length) {
 		JSSpec.logger.endRunner(this);
 	} else {
-		this.behaviors[this.index++].run();
+		window.setTimeout(behavior.run.bind(behavior), 0);
 	}
+	
+	this.index++;
 }
 
 
@@ -140,31 +176,41 @@ JSSpec.Runner.prototype.run = function() {
  * Logger
  */
 JSSpec.Logger = function() {
-	//TODO
+	this.summary = null;
+	this.log = null;
 }
 JSSpec.Logger.prototype.startRunner = function(runner) {
-	console.log("start runner");
-	console.log(runner);
-}
-JSSpec.Logger.prototype.endRunner = function(runner) {
-	console.log("end runner");
-	console.log(runner);
+	this.summary = document.createElement("DIV");
+	document.body.appendChild(this.summary);
+
+	this.log = document.createElement("DIV");
+	document.body.appendChild(this.log);
 }
 JSSpec.Logger.prototype.startBehavior = function(behavior) {
-	console.log("start behavior");
-	console.log(behavior);
-}
-JSSpec.Logger.prototype.endBehavior = function(behavior) {
-	console.log("end behavior");
-	console.log(behavior);
+	var heading = document.createElement("H2");
+	this.log.appendChild(heading);
+	heading.appendChild(document.createTextNode(behavior.context));
+	
+	var ul = document.createElement("UL");
+	this.log.appendChild(ul);
 }
 JSSpec.Logger.prototype.startExample = function(example) {
-	console.log("start example");
-	console.log(example);
 }
 JSSpec.Logger.prototype.endExample = function(example) {
-	console.log("end example");
-	console.log(example);
+	var ul = this.log.lastChild;
+	var li = document.createElement("LI");
+	ul.appendChild(li);
+	li.appendChild(document.createTextNode(example.name));
+	
+	var message
+	if(example.error) li.innerHTML += (" / " + example.error.message + " / line " + example.error.lineNumber);
+	if(example.failure) li.innerHTML += (" / " + example.failure.message + " / line " + example.failure.lineNumber);
+}
+JSSpec.Logger.prototype.endBehavior = function(behavior) {
+	
+}
+JSSpec.Logger.prototype.endRunner = function(runner) {
+	
 }
 
 
@@ -186,7 +232,9 @@ String.prototype.should = function() {
 					actual:self,
 					message: "Expected [" + expected + "] but [" + self + "]"
 				};
-				throw "AssertionFailure"
+				
+				if(JSSpec.Browser.Trident) JSSpec.example.firstPass = true;
+				throw "AssertionFailure";
 			}
 		}
 }
@@ -196,9 +244,10 @@ String.prototype.should = function() {
 if(JSSpec.Browser.Trident) {
 	window.onerror = function(ex, sourceUrl, lineNumber) {
 		var example = JSSpec.example;
-		
-		if(ex == "AssertionFailure") {
+
+		if(ex == "AssertionFailure" || example.firstPass) {
 			// Second pass
+			delete example.firstPass;
 			window.setTimeout(example.fn, 0);
 		} else if(example.failure) {
 			example.recordFailure(sourceUrl, lineNumber);
@@ -212,17 +261,9 @@ if(JSSpec.Browser.Trident) {
 	}
 }
 
-
-window.console = {
-	log: function(msg) {
-		var div = document.createElement("div");
-		div.innerHTML = msg;
-		document.body.appendChild(div);
-	}
-}
 window.onload = function() {
 	JSSpec.logger = new JSSpec.Logger();
 	JSSpec.runner = new JSSpec.Runner();
 	
-	JSSpec.runner.run();
+	window.setTimeout(JSSpec.runner.run.bind(JSSpec.runner));
 }
